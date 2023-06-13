4b5160e8a4a9976f29de8f27d8508a224a0b9dec
Vitor <vitor.duda@zenvia.com>
2023-06-13 09:05:41 -0300

    Adicionando método para autenticar usuário

--- src/main/java/br/com/unbhelp/services/UsuarioService.java
+++ src/main/java/br/com/unbhelp/services/UsuarioService.java
@@ -3,6 +3,8 @@ package br.com.unbhelp.services;
 import br.com.unbhelp.dao.UsuarioDAO;
 import br.com.unbhelp.entities.Usuario;
 import dtos.UsuarioDTO;
+import jakarta.security.auth.message.AuthException;
+import jakarta.ws.rs.NotAuthorizedException;
 import jakarta.ws.rs.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,18 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioDAO dao;
+
+    public Usuario autenticar(String matricula, String senha) throws AuthException {
+        final Usuario usuario = dao.findOneByMatricula(matricula);
+
+        if(usuario == null)
+            throw new NotAuthorizedException(String.format("Nenhum usuário encontrado com a matricula %s", matricula));
+
+        if(!usuario.getSenha().equals(senha))
+            throw new NotAuthorizedException(String.format("A senha %s está incorreta", senha));
+
+        return usuario;
+    }
 
     @Transactional
     public UsuarioDTO criarUsuario(UsuarioDTO dto) {
