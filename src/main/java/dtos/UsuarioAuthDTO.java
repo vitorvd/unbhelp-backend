e3a4564d976c3ba92d7a1218e5a7f8eb08c89735
Vitor <vitor.duda@zenvia.com>
2023-06-13 09:05:17 -0300

    Adicionando UsuarioAuthDTO

---
+++ src/main/java/dtos/UsuarioAuthDTO.java
@@ -0,0 +1,23 @@
+package dtos;
+
+import br.com.unbhelp.entities.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UsuarioAuthDTO {
+
+    private String token;
+
+    private String matricula;
+
+    public static UsuarioAuthDTO fromEntity(Usuario user, String token) {
+        UsuarioAuthDTO dto = UsuarioAuthDTO.builder().matricula(user.getMatricula()).token(token).build();
+
+        return dto;
+    }
+
+}
